8413b06940ab956966ca0c7e56a54adf0859265b
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-06-05 13:35:44 +0430

    Sample animation

--- main.cpp
+++ main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
-//#include <GL/freeglut.h>
 #include <GL/glut.h>
-#include <GL/glu.h>
-#include <GL/gl.h>
 
 using namespace std;
 
 void displayCB();
 void changeColor();
 void reshape(int w, int h);
+void animation(int pram);
 
 int main(int argc, char *argv[]) {
 
     glutInit(&argc, argv);
 
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
     glutInitWindowPosition(650, 250);
     glutInitWindowSize(500, 500);
 
-    glutCreateWindow("Hi");
+    glutCreateWindow("Sample Animation");
 
     glutDisplayFunc(displayCB);
     glutReshapeFunc(reshape);
+    glutTimerFunc(0, animation, 0);
     changeColor();
 
     glutMainLoop();
 
     return 0;
 }
+
+int step = 0;
 
 void displayCB() {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -44,11 +45,48 @@ void displayCB() {
         }
     }*/
     glBegin(GL_TRIANGLES);
-    glVertex2i(100, 100);
-    glVertex2i(100, -100);
-    glVertex2i(0, 0);
+
+    if ((step % 4) == 0) {
+        glVertex2i(0, 0);
+        glVertex2i(100, 0);
+        glVertex2i(50, 50);
+
+        glVertex2i(0, 0);
+        glVertex2i(-100, 0);
+        glVertex2i(-50, -50);
+
+    } else if ((step % 4) == 1) {
+        glVertex2i(0, 0);
+        glVertex2i(80, 80);
+        glVertex2i(30, 75);
+
+        glVertex2i(0, 0);
+        glVertex2i(-80, -80);
+        glVertex2i(-30, -75);
+    } else if ((step % 4) == 2) {
+        glVertex2i(0, 0);
+        glVertex2i(0, 100);
+        glVertex2i(-50, 50);
+
+        glVertex2i(0, 0);
+        glVertex2i(0, -100);
+        glVertex2i(50, -50);
+    } else if ((step % 4) == 3) {
+        glVertex2i(0, 0);
+        glVertex2i(-80, 80);
+        glVertex2i(-30, 25);
+
+        glVertex2i(0, 0);
+        glVertex2i(80, -80);
+        glVertex2i(30, -25);
+    }
+
+    step++;
+
     glEnd();
-    glFlush();
+
+//    glFlush();
+    glutSwapBuffers();
 }
 
 void changeColor() {
@@ -61,4 +99,9 @@ void reshape(int w, int h) {
     glLoadIdentity();
     gluOrtho2D(-250, 250, -250, 250);
     glMatrixMode(GL_MODELVIEW);
+}
+
+void animation(int pram) {
+    glutPostRedisplay();
+    glutTimerFunc(100, animation, 0);
 }
